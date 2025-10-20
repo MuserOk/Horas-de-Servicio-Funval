096947cc49301b08e704af799a6cc13cc1c5a377
NefiPineda <nefipinedagal@gmail.com>
2025-10-20 11:54:01 -0600

    uploading the loging page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Login from "./pages/login";
 
 export default function App() {
   return (
     <div>
-      
+      <Login />
     </div>
-  )
+  );
 }
